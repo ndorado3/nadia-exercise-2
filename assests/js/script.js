2315856095f55390d18e2f72e4e02d74249a9a53
nadiadorado@gmail.com <ndorado@uncc.edu>
2021-08-28 18:20:30 -0400

    CSS is done

---
+++ assests/js/script.js
@@ -0,0 +1,35 @@
+
+
+// function randomNumber(min, max) {
+//   return Math.floor(Math.random() * (max - min)) + min;
+// }
+
+// function generate() {
+//   document.getElementById('number').innerHTML = "Your number:" + randomNumber(1,100000);
+// }
+
+// function generate() {
+//   var numbers = [];
+//   for (var i = 0; i < 100; i++) {
+//     var randomNumber = Math.floor(Math.random() * 100 + 1);
+//     // if (randomNumber < 1 || randomNumber > 1000) alert("Too bad! You've lost!");
+//     console.log(randomNumber);
+//   }
+// }
+// document.getElementById("number").innerHTML = "Your number:" + number;
+
+// var playBtn = document.getElementById("playBtn");
+// // var number = doument.getElementById("number");
+
+// function generate() {
+//   //   var numbers = [];
+//   for (var i = 0; i < 12000; i++) {
+//     var randomNumber = Math.floor(Math.random() * 12000 + 1);
+//     // if (randomNumber < 1 || randomNumber > 1000)
+//     // window.confirm("Too bad! You've lost!");
+//     number.innerHTML = "<h3> Your number: " + randomNumber + "</h3>";
+//     // console.log(randomNumber);
+//   }
+// }
+
+// playBtn.addEventListener("click", generate);
