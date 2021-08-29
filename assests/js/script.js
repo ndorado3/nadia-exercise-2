116e988852f13c7071c9903ac760f5be54a4fb9f
nadiadorado@gmail.com <ndorado@uncc.edu>
2021-08-28 22:31:34 -0400

    the play button works

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,4 +1,45 @@
+var playBtn = document.getElementById("playBtn");
+var number = document.getElementById("number");
+var lostModal = document.getElementById("lostModal");
+var spanObject = document.getElementsByClassName("close")[0];
+var okBtn = document.getElementById("okBtn");
+// function
 
+spanObject.onclick = function () {
+  lostModal.style.display = "none";
+};
+
+okBtn.onclick = function () {
+  lostModal.style.display = "none";
+};
+
+lostModal.style.display = "none";
+
+function radomNumber() {
+  //   var numbers = [];
+  for (var i = 0; i < 30; i++) {
+    var numbers = Math.floor(Math.random() * 15000);
+    number.innerHTML = "<h3> Your number: " + numbers + "</h3>";
+  }
+  //   return number;
+  if (numbers < 1 || numbers > 1000) lostModal.style.display = "block";
+  else {
+    lostModal.style.display = "none";
+  }
+
+  //   if (numbers < 1 || numbers > 1000) alert("Too bad! You've lost!");
+}
+
+playBtn.addEventListener("click", radomNumber);
+
+// if (numbers < 1 || numbers > 1000) alert("Too bad! You've lost!");
+
+// var arr = [];
+// while(arr.length < 8){
+//     var r = Math.floor(Math.random() * 100) + 1;
+//     if(arr.indexOf(r) === -1) arr.push(r);
+// }
+// console.log(arr);
 
 // function randomNumber(min, max) {
 //   return Math.floor(Math.random() * (max - min)) + min;
@@ -19,7 +60,7 @@
 // document.getElementById("number").innerHTML = "Your number:" + number;
 
 // var playBtn = document.getElementById("playBtn");
-// // var number = doument.getElementById("number");
+// // var number = document.getElementById("number");
 
 // function generate() {
 //   //   var numbers = [];
